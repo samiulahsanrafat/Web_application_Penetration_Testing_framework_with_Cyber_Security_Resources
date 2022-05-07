7fd2443ee0b1c95044779887f0d6f64788655bf5
samiulahsanrafat <46289452+samiulahsanrafat@users.noreply.github.com>
2022-05-07 10:24:07 +0100

    Update main.py

--- lockdoors/main.py
+++ lockdoors/main.py
@@ -1,22 +1,13 @@
 import os
 import sys
 from pathlib import Path
-from lockdoors import infogathering
 from lockdoors import webhack
-from lockdoors import exploitation
-from lockdoors import reverse
-from lockdoors import encdyc
-from lockdoors import passattack
-from lockdoors import shells
-from lockdoors import privesc
-from lockdoors import soceng
-from lockdoors import psafrt
-from lockdoors import wtpp
 from lockdoors import about
 from lockdoors import update
 from lockdoors import shrts
 from datetime import datetime
 from time import sleep
+
 
 def menu():
     os.system('clear')
@@ -26,17 +17,8 @@ def menu():
 
             Make A Choice :\033[0m
 
-        \033[93m1)  Information Gathering
-        2)  Web Hacking
-        3)  Exploitation
-        4)  Reverse Engineering
-        5)  Encryption/Decryption
-        6)  Password Attacks
-        7)  Shells
-        8)  Privilege Escalation
-        9)  Social Engineering
-        10) Pentesting & Security Assessment Findings Report Templates
-        11) Help with Walk Throughs & Pentest Processing\033[0m
+        \033[93m1)  Web Hacking
+        \033[0m
         ------------------------
         \033[94ma)    About  Lockdoor
         u)    Update Lockdoor
@@ -45,27 +27,7 @@ def menu():
     choice = input("\033[92mLockdoor~# \033[0m")
     os.system('clear')
     if choice == "1":
-        infogathering.menu()
-    elif choice == "2":
         webhack.menu()
-    elif choice == "3":
-        exploitation.menu()
-    elif choice == "4":
-        reverse.menu()
-    elif choice == "5":
-        encdyc.menu()
-    elif choice == "6":
-        passattack.menu()
-    elif choice == "7":
-        shells.menu()
-    elif choice == "8":
-        privesc.menu()
-    elif choice == "9":
-        soceng.menu()
-    elif choice == "10":
-        psafrt.psafrt()
-    elif choice == "11":
-        wtpp.wtpp()
     elif choice == "a":
         about.show()
     elif choice == "u":
@@ -75,9 +37,11 @@ def menu():
         print("")
         now = datetime.now()
         date = now.strftime("%d/%m/%Y %H:%M:%S")
-        print("                 \033[91m-[!]- LOCKDOOR IS EXITING -[!]-\033[0m")
+        print(
+            "                 \033[91m-[!]- LOCKDOOR IS EXITING -[!]-\033[0m")
         print("")
-        print("                 \033[91m-[!]- EXITING AT " + date + " -[!]-\033[0m")
+        print(
+            "                 \033[91m-[!]- EXITING AT " + date + " -[!]-\033[0m")
         sys.exit()
     elif choice == "":
         menu()
